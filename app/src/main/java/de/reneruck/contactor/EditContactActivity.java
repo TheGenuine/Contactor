c46bb10e33bae689150ac1ab95fd75652d6a0a37
Rene Ruck <rene.ruck@eaglealpha.com>
2015-06-19 17:49:59 +0100

    save or update contact entry save button click

--- app/src/main/java/de/reneruck/contactor/EditContactActivity.java
+++ app/src/main/java/de/reneruck/contactor/EditContactActivity.java
@@ -1,11 +1,13 @@
 package de.reneruck.contactor;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import de.reneruck.contactor.models.Contact;
 
@@ -45,12 +47,41 @@ public class EditContactActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id){
+            case R.id.action_save:
+                if(updateContact()){
+                    Toast.makeText(getApplicationContext(), "Contact saved", Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Couldn't save this contact, try again", Toast.LENGTH_LONG).show();
+                }
         }
 
-        return super.onOptionsItemSelected(item);
+        return true;
+    }
+
+    private boolean updateContact(){
+        String name = ((EditText) findViewById(R.id.input_name)).getText().toString();
+
+        String[] nameSplit = name.split(" ");
+
+        String email = ((EditText) findViewById(R.id.input_email)).getText().toString();
+        String phone_priv = ((EditText) findViewById(R.id.input_phone_private)).getText().toString();
+        String phone_work = ((EditText) findViewById(R.id.input_phone_work)).getText().toString();
+
+        ContentValues values = new ContentValues();
+        values.put("firstName", nameSplit[0]);
+        values.put("lastName", nameSplit[1]);
+        values.put("email", email);
+        values.put("phonePrivate", phone_priv);
+        values.put("phoneWork", phone_work);
+
+        String[] args = {String.valueOf(this.currentContact.getId())};
+        if(this.currentContact.getId() != -1){
+            return this.sqlHelper.getWritableDatabase().update(ContactsOpenHelper.CONTACTS_TABLE_NAME, values, "where _id = ?", args) > 0 ? true : false;
+        } else {
+            return this.sqlHelper.getWritableDatabase().insert(ContactsOpenHelper.CONTACTS_TABLE_NAME, null, values) > 0 ? true : false;
+        }
     }
 
     private void populateFields() {
