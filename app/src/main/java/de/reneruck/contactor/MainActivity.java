282ac2cd1ac75582f4a0ee29b29fdaa6c7482ad7
Rene Ruck <rene.ruck@eaglealpha.com>
2015-06-19 17:06:23 +0100

    add click listener for add contact button when list is empty

--- app/src/main/java/de/reneruck/contactor/MainActivity.java
+++ app/src/main/java/de/reneruck/contactor/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button_add_contact).setOnClickListener(add_contact_listener);
     }
 
     @Override
@@ -34,4 +37,11 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private View.OnClickListener add_contact_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "Add new Contact", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
