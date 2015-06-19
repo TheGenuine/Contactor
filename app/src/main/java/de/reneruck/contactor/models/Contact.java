82a9fa3eae44aecc3d6b19cf40411be2cce4308f
Rene Ruck <rene.ruck@eaglealpha.com>
2015-06-19 17:27:28 +0100

    define local fields with getters and setters

--- app/src/main/java/de/reneruck/contactor/models/Contact.java
+++ app/src/main/java/de/reneruck/contactor/models/Contact.java
@@ -4,4 +4,63 @@ package de.reneruck.contactor.models;
  * Created by reneruck on 19/06/15.
  */
 public class Contact {
+
+    private int id = -1;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneWork;
+    private String phonePrivate;
+
+    public String getName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneWork() {
+        return phoneWork;
+    }
+
+    public String getPhonePrivate() {
+        return phonePrivate;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneWork(String phoneWork) {
+        this.phoneWork = phoneWork;
+    }
+
+    public void setPhonePrivate(String phonePrivate) {
+        this.phonePrivate = phonePrivate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
