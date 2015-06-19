b048e272c3ec1704cc110137b1ead6a363bf542b
Rene Ruck <rene.ruck@eaglealpha.com>
2015-06-19 17:48:36 +0100

    load menu definition in EditContactActivity

--- app/src/main/java/de/reneruck/contactor/EditContactActivity.java
+++ app/src/main/java/de/reneruck/contactor/EditContactActivity.java
@@ -3,6 +3,8 @@ package de.reneruck.contactor;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import de.reneruck.contactor.models.Contact;
@@ -27,6 +29,28 @@ public class EditContactActivity extends AppCompatActivity {
             loadContactData();
             populateFields();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit_contact, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void populateFields() {
